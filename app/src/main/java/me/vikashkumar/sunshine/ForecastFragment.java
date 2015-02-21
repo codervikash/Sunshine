80d0b78dec0c38ae85a755ff0a8fe89cf2cc2837
Vikash Kumar <mailkumarvikash@gmail.com>
2015-02-22 01:14:09 +0530

    added toast

--- app/src/main/java/me/vikashkumar/sunshine/ForecastFragment.java
+++ app/src/main/java/me/vikashkumar/sunshine/ForecastFragment.java
@@ -11,8 +11,10 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -102,6 +104,14 @@ public class ForecastFragment extends Fragment {
         // Get a reference to the ListView, and attach this adapter to it.
         ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
         listView.setAdapter(mforecastAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view,int position, long l) {
+                String forecast = mforecastAdapter.getItem(position);
+                Toast.makeText(getActivity(), forecast,Toast.LENGTH_SHORT).show(); 
+
+            }
+        });
 
         return rootView;
     }
@@ -284,11 +294,11 @@ public class ForecastFragment extends Fragment {
             if(result!=null){
                 mforecastAdapter.clear();
                 for(String dayForecastStr:result){
-                    mforecastAdapter.add(dayForecastStr);
+                    mforecastAdapter.addAll(dayForecastStr);
 
                 }
             }else {
-               Log.v(LOG_TAG, "sdfddf");
+               Log.v(LOG_TAG, "No return Value");
             }
 
         }

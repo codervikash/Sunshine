1ae6933cacba806810c852b93a8050f5ff4136a6
Vikash Kumar <mailkumarvikash@gmail.com>
2015-03-16 23:48:52 +0530

    added content provider

--- app/src/main/java/me/vikashkumar/sunshine/ForecastFragment.java
+++ app/src/main/java/me/vikashkumar/sunshine/ForecastFragment.java
@@ -3,11 +3,8 @@ package me.vikashkumar.sunshine;
 import android.app.Fragment;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -18,19 +15,7 @@ import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.Date;
 
 /**
  * Created by vikashkumar on 21/02/15.
@@ -70,7 +55,7 @@ public class ForecastFragment extends Fragment {
 
 
     private void updateWeather() {
-        FetchWeatherTask weatherTask= new FetchWeatherTask();
+        FetchWeatherTask weatherTask= new FetchWeatherTask(getActivity());
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
         String location = preferences.getString(getString(R.string.pref_location_key), getString(R.string.pref_location_default));
 
